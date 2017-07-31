994f34033bbfed74446e93bdfd6adf9031888ecd
cgm616 <cgm616@me.com>
2017-07-31 11:53:58 -0400

    Fix build error with javascript

--- src/js/app.js
+++ src/js/app.js
@@ -14,22 +14,23 @@ if ('serviceWorker' in navigator) {
 window.onload = function() {
   var x = document.getElementsByTagName("a");
   var i;
-      for (i = 0; i < x.length; i++) {
-        var href = x[i].getAttribute("href");
-        if(href.substr(0,1) == '/' && href !== window.location.pathname) {
-          x[i].addEventListener("mouseenter", (function() {
-            if (!navigator.serviceWorker.controller) {
-              console.log("no controller");
-              return;
-            }
-            navigator.serviceWorker.controller.postMessage({
-              url: this.getAttribute('href'),
-              command: 'fetch'
-            });
-          }),
-            { 'once': true, 'passive': true, }
-          );
-        }
+  if('serviceWorker' in navigator) {
+    for (i = 0; i < x.length; i++) {
+      var href = x[i].getAttribute("href");
+      if(href.substr(0,1) == '/' && href !== window.location.pathname) {
+        x[i].addEventListener("mouseenter", (function() {
+          if (!navigator.serviceWorker.controller) {
+            console.log("no controller");
+            return;
+          }
+          navigator.serviceWorker.controller.postMessage({
+            url: this.getAttribute('href'),
+            command: 'fetch'
+          });
+        }),
+          { 'once': true, 'passive': true, }
+        );
       }
-    }
+    } 
+  } 
 };
