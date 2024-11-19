7585c3d6f9c49fa2b21ce21ca7d51f740c11b1b7
cgm616 <cgm616@users.noreply.github.com>
2024-11-19 07:39:44 +0000

    deploy: c7beae00240abfcc60afd21d607acb5837d34926

--- js/app.min.39cd2fc91b96413ddc67372382e6de71351c4312daf587dbce6456f3c67e6ae0e0c8cb6e3a71f80a795f204e686fe0bb256b6a1e0401d668fa7c97fdbcb3480b.js
+++
@@ -1,1 +0,0 @@
-"use strict";var CACHE="cache";"serviceWorker"in navigator&&navigator.serviceWorker.register("/sw.js",{scope:"/"}).then(function(e){console.log("Registration succeeded. Scope is "+e.scope)}).catch(function(e){console.log("Registration failed with "+e)})
